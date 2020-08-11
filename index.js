b10204e18d97a175e7214de739d841503a28328f
Jeffrey Vest <jeffv@aspenrxhealth.com>
2020-08-11 14:17:11 -0400

    Added buttons and gear

---
+++ index.js
@@ -0,0 +1,10 @@
+var prodCount = 50;
+
+function prodInc(count) {
+    prodCount += count;
+    updateProdCount();
+}
+function updateProdCount() {
+    var prodCountSpan = document.getElementsByClassName("prod-count");
+    prodCountSpan[0].innerHTML = prodCount.toString();
+}
