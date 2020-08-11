682b93afcad801b6de770ae1b9ae5de42449b81a
Jeffrey Vest <jeffv@aspenrxhealth.com>
2020-08-11 16:17:17 -0400

    Fixed title

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function prodInc(count) {
     prodCount += count;
     updateProdCount();
 }
+
 function updateProdCount() {
     var prodCountSpan = document.getElementsByClassName("prod-count");
     prodCountSpan[0].innerHTML = prodCount.toString();
